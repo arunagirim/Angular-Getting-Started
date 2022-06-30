1c41094e4492212b2ca0e7dbf307cef20eb0fefe
arunagirim <arunagiri350@gmail.com>
2022-06-30 23:50:38 +0530

    List Filtered feature added

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,18 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
 
 @Component({
   selector: 'pm-products',
   templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
   pageTitle: string = 'Product List';
   imageWidth: number = 50;
   imageMargin: number = 2;
+  showImage: boolean = false;
 
-  products: any[] = [
+  private _listFilter: string = '';
+
+  get listFilter(): string {
+    return this._listFilter;
+  }
+
+  set listFilter(value: string) {
+    this._listFilter = value;
+    console.log('In setter : ', value);
+    this.filteredProducts = this.performFilter(value);
+  }
+
+  products: IProduct[] = [
     {
       productId: 2,
-      productName: 'Product 2',
+      productName: 'Apple Photo',
       productCode: 'PD-02',
       releaseDate: 'March 18, 2022',
       description: 'Product 2 Product 2 Product 2, Product 2',
@@ -22,7 +37,7 @@ export class ProductListComponent {
     },
     {
       productId: 4,
-      productName: 'Product 4',
+      productName: 'Organic pinewine',
       productCode: 'PD-04',
       releaseDate: 'Apr 18, 2022',
       description: 'Product 4 Product 4 Product 4, Product 4',
@@ -31,4 +46,21 @@ export class ProductListComponent {
       imageUrl: 'assets/img/tab.jpg',
     },
   ];
+
+  filteredProducts: IProduct[] = this.products;
+
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLocaleLowerCase().includes(filterBy)
+    );
+  }
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
+
+  ngOnInit(): void {
+    console.log('In ngOnInit');
+  }
 }
